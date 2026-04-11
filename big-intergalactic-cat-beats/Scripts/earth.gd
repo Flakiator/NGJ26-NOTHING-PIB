c40c99b7e25d3b753a38a126fab29b30124c1eec
Niklas <nizc@itu.dk>
2026-04-11 21:14:48 +0200

    make planet sprite folder and add dummy code for overwriting sprite texture

--- big-intergalactic-cat-beats/Scripts/earth.gd
+++ big-intergalactic-cat-beats/Scripts/earth.gd
@@ -5,5 +5,6 @@ func _ready() -> void:
 	var scaling_factor = randf_range(0.07,0.2)
 	random_speed = randf_range(0.1, 0.3)
 	global_scale = Vector2(scaling_factor, scaling_factor)
+	texture = load("res://Sprites/earth.png")
 func _process(delta: float) -> void:
 	self.rotate(delta*random_speed)
