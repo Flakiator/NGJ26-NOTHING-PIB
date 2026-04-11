644500e098de5a4cd5aa2d494e110057563c60c6
Niklas <nizc@itu.dk>
2026-04-11 20:36:44 +0200

    fix stars dissapearing

--- big-intergalactic-cat-beats/Scripts/star.gd
+++ big-intergalactic-cat-beats/Scripts/star.gd
@@ -7,9 +7,11 @@ func _ready() -> void:
 	speed_fraction = (30 + randi() % 30) / 100.0
 	var scaling_factor = randf_range(0.02,0.05)
 	global_scale = Vector2(scaling_factor, scaling_factor)
+	var cam = get_tree().get_first_node_in_group("camera")
+	cam_pos = cam.global_position
 	
 func _process(delta: float) -> void:
 	var cam = get_tree().get_first_node_in_group("camera")
-	var speed = cam.global_position - cam_pos 
-	global_position = global_position + speed * speed_fraction
+	var speed = cam.global_position - cam_pos
+	self.transform = self.transform.translated(speed * speed_fraction)
 	cam_pos = cam.global_position
