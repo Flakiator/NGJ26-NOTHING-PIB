4df55d7a93fe30e490310be29ec287258d580c1e
Sebastian Cloos Hylander <sehy@itu.dk>
2026-04-11 19:10:20 +0200

    fixed planet_groups

--- big-intergalactic-cat-beats/Scripts/spawn_manager.gd
+++ big-intergalactic-cat-beats/Scripts/spawn_manager.gd
@@ -5,7 +5,7 @@ extends Node2D
 @export var no_of_planets: int
 @export var spawn_distance: int
 @export var camera: Camera2D
-
+@export var planet_group: String
 
 
 # Called when the node enters the scene tree for the first time.
@@ -30,7 +30,7 @@ func spawn_planet(attempt = 0):
 		return
 	if get_tree().get_first_node_in_group("player").planet: # no spawning if cat is on planet
 		return
-	var planets = get_tree().get_nodes_in_group("planets") 
+	var planets = get_tree().get_nodes_in_group(planet_group) 
 	if  planets.size() < no_of_planets:
 		var spawners = get_tree().get_nodes_in_group("spawner")
 		#var spawner = spawners[get_spawner_from_direction()] # Spawn planet in the direction the player is heading
@@ -53,7 +53,7 @@ func spawn_planet(attempt = 0):
 	
 func cleanup_planets():
 	var cam_pos = camera.global_position
-	for planet in get_tree().get_nodes_in_group("planets"):
+	for planet in get_tree().get_nodes_in_group(planet_group):
 		if planet.global_position.distance_to(cam_pos) > 1000:
 			planet.queue_free()
 
