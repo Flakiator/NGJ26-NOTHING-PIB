baa66cfcd3d78e96a83b27eb80244d7e0b086bfd
Sebastian Cloos Hylander <sehy@itu.dk>
2026-04-11 19:29:21 +0200

    slow star movement

---
+++ big-intergalactic-cat-beats/Scenes/star.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+var speed_fraction : float
+var cam_pos : Vector2
+
+func _ready() -> void:
+	speed_fraction = (30 + randi() % 30) / 100.0
+	
+func _process(delta: float) -> void:
+	var cam = get_tree().get_first_node_in_group("camera")
+	var speed = cam.global_position - cam_pos 
+	self.transform = self.transform.translated(speed * speed_fraction)
+	cam_pos = cam.global_position
