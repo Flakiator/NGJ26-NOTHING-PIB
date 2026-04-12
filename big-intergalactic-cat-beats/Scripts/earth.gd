19bdcb1761aa7e0d7dd9c129203f86fb93341961
Niklas <nizc@itu.dk>
2026-04-12 14:56:20 +0200

    add planet scenes

--- big-intergalactic-cat-beats/Scripts/earth.gd
+++ big-intergalactic-cat-beats/Scripts/earth.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 	var scaling_factor = randf_range(0.07,0.2)
 	random_speed = randf_range(0.1, 0.3)
 	global_scale = Vector2(scaling_factor, scaling_factor)
-	#texture = get_random_planet()
+	texture = get_random_planet()
 func _process(delta: float) -> void:
 	self.rotate(delta*random_speed)
 	
