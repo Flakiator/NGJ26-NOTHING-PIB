4b6ba9ecd94c3823fbfff6f0fa79425deae327b4
Sebastian Cloos Hylander <sehy@itu.dk>
2026-04-12 13:57:03 +0200

    music fade in intro

--- big-intergalactic-cat-beats/Scripts/main_theme.gd
+++ big-intergalactic-cat-beats/Scripts/main_theme.gd
@@ -1,5 +1,14 @@
 extends AudioStreamPlayer
 
+func _ready() -> void:
+	fade_in()
+
+func fade_in():
+	playing = true
+	for i in range(20):
+		volume_db += 1
+		await get_tree().create_timer(.1).timeout
+
 func fade_out():
 	for i in range(20):
 		volume_db -= 1
